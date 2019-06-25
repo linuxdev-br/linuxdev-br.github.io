4ac037690787fca78a3ee6c6dda9518137decc43
Gustavo Padovan <gustavo.padovan@collabora.com>
2019-06-25 10:59:40 -0300

    Updates

--- component---src-pages-news-js-e96db663378f816ad3cb.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{141:function(e,n,a){"use strict";a.r(n);var t=a(0),l=a.n(t),u=a(156),c=a(168),r=a(165),o=a(167);n.default=function(){return l.a.createElement(u.a,null,l.a.createElement(c.a,null),l.a.createElement(r.a,null),l.a.createElement(o.a,null))}}}]);
-//# sourceMappingURL=component---src-pages-news-js-e96db663378f816ad3cb.js.map
